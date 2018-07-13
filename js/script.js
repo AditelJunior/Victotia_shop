9f7d7c4e8d1b873ff4733be117a1be5b7381a647
Adilet <aitmatov40@gmail.com>
2018-07-14 01:22:07 +0600

    remained adaptation

--- js/script.js
+++ js/script.js
@@ -1,19 +1,13 @@
-$(function () {
-    $('.owl-carousel').owlCarousel()
-})
-// {
-//     loop: true,
-//         margin: 10,
-//             nav: true,
-//                 responsive: {
-//         0: {
-//             items: 1
-//         },
-//         600: {
-//             items: 3
-//         },
-//         1000: {
-//             items: 5
-//         }
-//     }
-// }
+var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
